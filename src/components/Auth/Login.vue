1fb2acb6fa7328b43ec278ea9fea53da5603ec9b
Evgen904 <evgen904@gmail.com>
2019-01-13 15:31:16 +0300

    Создание страницы логина

--- src/components/Auth/Login.vue
+++ src/components/Auth/Login.vue
@@ -1,8 +1,45 @@
 <template>
-  <v-container>
-    <v-layout row>
-      <v-flex xs12>
-        <h1>Login</h1>
+  <v-container fluid fill-height>
+    <v-layout align-center justify-center>
+      <v-flex xs12 sm8 md6>
+        <v-card class="elevation-12">
+          <v-toolbar dark color="primary">
+            <v-toolbar-title>Login form</v-toolbar-title>
+          </v-toolbar>
+          <v-card-text>
+            <v-form
+              ref="form"
+              v-model="valid"
+              validation
+            >
+              <v-text-field
+                prepend-icon="person"
+                name="email"
+                label="email"
+                type="email"
+                v-model="email"
+                :rules="emailRules"
+              ></v-text-field>
+              <v-text-field
+                prepend-icon="lock"
+                name="password"
+                label="Password"
+                type="password"
+                v-model="password"
+                :counter="6"
+                :rules="passwordRules"
+              ></v-text-field>
+            </v-form>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+              color="primary"
+              @click="onSubmit"
+              :disabled="!valid"
+            >Login</v-btn>
+          </v-card-actions>
+        </v-card>
       </v-flex>
     </v-layout>
   </v-container>
@@ -11,7 +48,32 @@
 <script>
   export default {
     data () {
-      return {}
+      return {
+        email: '',
+        password: '',
+        valid: false,
+        emailRules: [
+          v => !!v || 'E-mail is required',
+          v => /.+@.+/.test(v) || 'E-mail must be valid'
+        ],
+        passwordRules: [
+          v => !!v || 'Password is required',
+          v => (v && v.length >= 6) || 'Password must be equal or more than 6 characters'
+        ]
+      }
+    },
+    methods: {
+      onSubmit () {
+        if (this.$refs.form.validate()) {
+          const user = {
+            email: this.email,
+            password: this.password
+          }
+
+          console.log(user);
+
+        }
+      }
     }
   }
 </script>
