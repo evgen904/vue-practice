7121eed6e69e116ed6c70e6d49a5b35362f2281a
Evgen904 <evgen904@gmail.com>
2019-01-19 23:23:44 +0300

    Создание страницы объявления

--- src/components/Ads/Ad.vue
+++ src/components/Ads/Ad.vue
@@ -2,7 +2,21 @@
   <v-container>
     <v-layout row>
       <v-flex xs12>
-        <h1>Ad</h1>
+        <v-card>
+          <v-card-media
+            src="https://cdn.vuetifyjs.com/images/carousel/sky.jpg"
+            height="300"
+          ></v-card-media>
+          <v-card-text>
+            <h2 class="text--primary">Lorem</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, eius.</p>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn class="warning" flat>Edit</v-btn>
+            <v-btn class="success">Buy</v-btn>
+          </v-card-actions>
+        </v-card>
       </v-flex>
     </v-layout>
   </v-container>
