961287932b214b5308c35f32ad16a5e85dfeb8d8
Evgen904 <evgen904@gmail.com>
2019-01-20 01:11:48 +0300

    Настройка firebase

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App'
 import router from './router'
 import store from './store'
 import Vuetify from 'vuetify'
+import * as fb from 'firebase'
 import 'vuetify/dist/vuetify.min.css'
 
 Vue.use(Vuetify)
@@ -15,5 +16,15 @@ new Vue({
   router,
   store,
   components: { App },
-  template: '<App/>'
+  template: '<App/>',
+  created () {
+    fb.initializeApp({
+      apiKey: 'AIzaSyC-EVj6IGepD3F6pEuECX6Gkgbx9ikBloU',
+      authDomain: 'its-ads-4c326.firebaseapp.com',
+      databaseURL: 'https://its-ads-4c326.firebaseio.com',
+      projectId: 'its-ads-4c326',
+      storageBucket: 'its-ads-4c326.appspot.com',
+      messagingSenderId: '242674476104'
+    })
+  }
 })
