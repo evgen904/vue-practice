269f79e189c436d5bc1e50a2b7484fe8ae312ffb
Evgen904 <evgen904@gmail.com>
2019-01-20 20:32:53 +0300

    Вывод ошибок

--- src/App.vue
+++ src/App.vue
@@ -49,6 +49,26 @@
     <v-content>
       <router-view></router-view>
     </v-content>
+
+    <template v-if="error">
+      <v-snackbar
+        :timeout="5000"
+        :multi-line="true"
+        color="error"
+        @input="closeError"
+        :value="true"
+      >
+        {{error}}
+        <v-btn
+          dark
+          flat
+          @click.native="closeError"
+        >
+          Close
+        </v-btn>
+      </v-snackbar>
+    </template>
+
   </v-app>
 </template>
 
@@ -64,6 +84,16 @@ export default {
         {title: 'New ad', icon: 'note_add', url: '/new'},
         {title: 'my ads', icon: 'list', url: '/list'}
       ]
+    }
+  },
+  computed: {
+    error () {
+      return this.$store.getters.error
+    }
+  },
+  methods: {
+    closeError () {
+      this.$store.dispatch('clearError')
     }
   }
 }
