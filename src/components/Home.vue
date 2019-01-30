fe6af4457c3e48902fc93b1cad76f3cecae3c37f
Evgen904 <evgen904@gmail.com>
2019-01-31 00:17:24 +0300

    Загрузка объявлений по умолчанию

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div v-if="!loading">
     <v-container fluid>
       <v-layout row>
         <v-flex xs12>
@@ -53,6 +53,20 @@
       </v-layout>
     </v-container>
   </div>
+  <div v-else>
+    <v-container>
+      <v-layout row>
+        <v-flex xs12 class="text-xs-center pt-5">
+          <v-progress-circular
+            :size="100"
+            :width="4"
+            color="purple"
+            indeterminate
+          ></v-progress-circular>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
 </template>
 
 <script>
@@ -63,6 +77,9 @@
       },
       ads () {
         return this.$store.getters.ads
+      },
+      loading () {
+        return this.$store.getters.loading
       }
     }
   }
--- src/main.js
+++ src/main.js
@@ -37,5 +37,7 @@ new Vue({
         this.$store.dispatch('autoLoginUser', user)
       }
     })
+
+    this.$store.dispatch('fetchAds')
   }
 })
